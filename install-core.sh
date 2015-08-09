9a1be21456f4885081371e4fe590b2f806c470f8
Edward Hernandez <edward.hernandez@gmx.com>
2015-08-08 20:54:09 -0400

    split and update install scripts

--- install-core.sh
+++ install-core.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
-sudo apt-add-repository -y ppa:lvillani/i3
-sudo apt-add-repository ppa:snwh/pulp
-sudo apt-add-repository ppa:numix/ppa
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+# sudo apt-add-repository -y ppa:snwh/pulp
+# sudo apt-add-repository -y ppa:numix/ppa
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+# echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install git vim i3 zsh vlc gnome-core i3-gnome paper-gtk-theme paper-icon-theme numix-icon-theme libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev dconf-editor gconf-editor gnome-tweak-tool pcmanfm spotify-client ghc cabal-install
+sudo apt-get install -y git vim i3 zsh vlc paper-gtk-theme paper-icon-theme numix-icon-theme libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev dconf-editor gconf-editor gnome-tweak-tool pcmanfm spotify-client ghc cabal-install
 cabal update
 cabal install shelly
 cd ~
-mkdir ~/.builds ~/.vim ~/.vim/bundle ~/.zsh ~/.i3 ~/.config/zathura ~/.config/dunst
+mkdir ~/.builds ~/.vim ~/.vim/bundle ~/.zsh ~/.i3 ~/.config/zathura ~/.config/dunst ~/.config/xfce4 ~/.config/xfce4/xfconf ~/.config/xfce4/xconf/xfce-perchannel-xml
 git clone https://github.com/Airblader/i3 ~/.builds/i3-gaps
 git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
 git clone https://github.com/Tarrasch/antigen-hs.git ~/.zsh/antigen-hs/
@@ -23,6 +22,7 @@ make && sudo make install
 cd ~
 sudo cp ~/.builds/flattr-icons/Flattr /usr/share/icons/
 cp ~/.dotfiles/muttrc ~/.muttrc
+ln -f ~/.dotfiles/xfce/* ~.config/xfce4/xconf/xfce-perchannel-xml/
 ln ~/.dotfiles/i3 ~/.i3/config
 ln ~/.dotfiles/zshrc ~/.zshrc
 ln ~/.dotfiles/vimrc ~/.vimrc
@@ -35,7 +35,6 @@ ln ~/.dotfiles/MyAntigen.hs ~/.zsh/MyAntigen.hs
 sudo ln -s ~/.builds/pure/async.zsh /usr/share/zsh/site-functions/async
 sudo ln -s ~/.builds/pure/pure.zsh /usr/share/zsh/site-functions/prompt_pure_setup
 cd
-chmod +x ~/.dotfiles/ashes.sh
 ./.dotfiles/ashes.sh
 sudo apt-get autoremove
 chsh -s $(which zsh)
