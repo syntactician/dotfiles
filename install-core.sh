8d433bb864abe2665c4269c8126505d6de96899d
Edward Hernandez <ehernandez@email.wm.edu>
2015-08-08 21:18:43 -0400

    updated directory structure

--- install-core.sh
+++ install-core.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
-# sudo apt-add-repository -y ppa:snwh/pulp
-# sudo apt-add-repository -y ppa:numix/ppa
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
-# echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+# this assumes that this repo is cloned to ~/.dotfiles and is only tested on Ubuntu 14.04 - 15.10
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install -y git vim i3 zsh vlc paper-gtk-theme paper-icon-theme numix-icon-theme libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev dconf-editor gconf-editor gnome-tweak-tool pcmanfm spotify-client ghc cabal-install
+sudo apt-get install -y vim i3 zsh vlc paper-gtk-theme paper-icon-theme numix-icon-theme libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev dconf-editor gconf-editor gnome-tweak-tool pcmanfm spotify-client ghc cabal-install
 cabal update
 cabal install shelly
 cd ~
@@ -12,8 +9,8 @@ mkdir ~/.builds ~/.vim ~/.vim/bundle ~/.zsh ~/.i3 ~/.config/zathura ~/.config/du
 git clone https://github.com/Airblader/i3 ~/.builds/i3-gaps
 git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
 git clone https://github.com/Tarrasch/antigen-hs.git ~/.zsh/antigen-hs/
-git clone https://github.com/sindresorhus/pure.git ~/.builds/
-git clone https://github.com/NitruxSA/flattr-icons ~/.builds/
+git clone https://github.com/sindresorhus/pure.git ~/.builds/pure
+git clone https://github.com/NitruxSA/flattr-icons ~/.builds/flattr-icons
 git clone https://github.com/syntactician/dotfiles.git ~/.dotfiles
 git config --global user.email "edward.hernandez@gmx.com"
 git config --global user.name "Edward Hernandez"
