576db93401cc9fcf2a6bb06052061bae87b8c8f8
Edward Hernandez <edward.hernandez@gmx.com>
2015-08-09 01:26:37 -0400

    working version 1.0

--- configure.zsh
+++ configure.zsh
@@ -1,2 +1,9 @@
-#! /bin/zsh
+#! /bin/bash
+apt-add-repository -y ppa:snwh/pulp
+apt-add-repository -y ppa:numix/ppa
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+apt-get update && sudo apt-get upgrade -y
+apt-get install -y numix-icon-theme paper-icon-theme paper-gtk-theme spotify-client
+
 antigen-hs-compile
--- install-extra.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo apt-add-repository -y ppa:snwh/pulp
-sudo apt-add-repository -y ppa:numix/ppa
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y numix-icon-theme paper-icon-theme paper-gtk-theme spotify-client
