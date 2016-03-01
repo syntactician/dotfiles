b0330d5f88f5ad2ba85ec03d500a81fd0fe5fd3c
Edward Hernandez <ehernandez@email.wm.edu>
2016-03-01 10:36:01 -0500

    started arch script

--- install.sh
+++ install.sh
@@ -1,39 +1,58 @@
-#! /bin/bash
+#!/bin/sh
 # this assumes that this repo is cloned to ~/.dotfiles and is only tested on Ubuntu 14.04 - 15.10
-apt-get update && sudo apt-get upgrade
-apt-get install vim i3 zsh vlc libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev dconf-editor gconf-editor gnome-tweak-tool gnome-terminal rxvt-unicode pcmanfm ghc cabal-install
-cabal update
-cabal install shelly
-cd ~
-mkdir /usr/share/zsh/site-functions ~/.builds ~/.vim ~/.vim/bundle ~/.zsh ~/.i3 ~/.config/zathura ~/.config/dunst ~/.config/xfce4 ~/.config/xfce4/xfconf ~/.config/xfce4/xfconf/xfce-perchannel-xml
-cd
-git clone https://github.com/Airblader/i3 ~/.builds/i3-gaps
-git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
-git clone https://github.com/Tarrasch/antigen-hs.git ~/.zsh/antigen-hs/
-git clone https://github.com/sindresorhus/pure.git ~/.builds/pure
-git clone https://github.com/NitruxSA/flattr-icons ~/.builds/flattr-icons
-git clone https://github.com/syntactician/dotfiles.git ~/.dotfiles
-git config --global user.email "edward.hernandez@gmx.com"
-git config --global user.name "Edward Hernandez"
-cd ~/.builds/i3-gaps
-make && sudo make install
-cd ~
-cp -r ~/.builds/flattr-icons/Flattr /usr/share/icons/
-# cp ~/.dotfiles/muttrc ~/.muttrc
-ln -f ~/.dotfiles/xfce/* ~/.config/xfce4/xconf/xfce-perchannel-xml/
-ln ~/.dotfiles/i3 ~/.i3/config
-ln ~/.dotfiles/zshrc ~/.zshrc
-ln ~/.dotfiles/vimrc ~/.vimrc
-ln ~/.dotfiles/wallpaper.png ~/Pictures/
-ln ~/.dotfiles/zathurarc ~/.config/zathura/zathurarc
-ln ~/.dotfiles/dunstrc ~/.config/dunst/dunstrc
-ln ~/.dotfiles/Xresources ~/.Xresources
-ln ~/.dotfiles/README.md ~/todo.md 
-ln ~/.dotfiles/MyAntigen.hs ~/.zsh/MyAntigen.hs
-ln -s ~/.builds/pure/async.zsh /usr/local/share/zsh/site-functions/async
-ln -s ~/.builds/pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup
-cd
-./.dotfiles/ashes.sh
-apt-get autoremove
-chsh -s $(which zsh)
-cd
+# apt-get update && sudo apt-get upgrade
+# apt-get install vim i3 zsh vlc libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev dconf-editor gconf-editor gnome-tweak-tool gnome-terminal rxvt-unicode pcmanfm ghc cabal-install
+# cabal update
+# cabal install shelly
+# cd ~
+# mkdir /usr/share/zsh/site-functions ~/.builds ~/.vim ~/.vim/bundle ~/.zsh ~/.i3 ~/.config/zathura ~/.config/dunst ~/.config/xfce4 ~/.config/xfce4/xfconf ~/.config/xfce4/xfconf/xfce-perchannel-xml
+# cd
+# git clone https://github.com/Airblader/i3 ~/.builds/i3-gaps
+# git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
+# git clone https://github.com/Tarrasch/antigen-hs.git ~/.zsh/antigen-hs/
+# git clone https://github.com/sindresorhus/pure.git ~/.builds/pure
+# git clone https://github.com/NitruxSA/flattr-icons ~/.builds/flattr-icons
+# git clone https://github.com/syntactician/dotfiles.git ~/.dotfiles
+# git config --global user.email "edward.hernandez@gmx.com"
+# git config --global user.name "Edward Hernandez"
+# cd ~/.builds/i3-gaps
+# make && sudo make install
+# cd ~
+# cp -r ~/.builds/flattr-icons/Flattr /usr/share/icons/
+# # cp ~/.dotfiles/muttrc ~/.muttrc
+# ln -f ~/.dotfiles/xfce/* ~/.config/xfce4/xconf/xfce-perchannel-xml/
+# ln ~/.dotfiles/i3 ~/.i3/config
+# ln ~/.dotfiles/zshrc ~/.zshrc
+# ln ~/.dotfiles/vimrc ~/.vimrc
+# ln ~/.dotfiles/wallpaper.png ~/Pictures/
+# ln ~/.dotfiles/zathurarc ~/.config/zathura/zathurarc
+# ln ~/.dotfiles/dunstrc ~/.config/dunst/dunstrc
+# ln ~/.dotfiles/Xresources ~/.Xresources
+# ln ~/.dotfiles/README.md ~/todo.md 
+# ln ~/.dotfiles/MyAntigen.hs ~/.zsh/MyAntigen.hs
+# ln -s ~/.builds/pure/async.zsh /usr/local/share/zsh/site-functions/async
+# ln -s ~/.builds/pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup
+# cd
+# ./.dotfiles/ashes.sh
+# apt-get autoremove
+# chsh -s $(which zsh)
+# cd
+
+# an Arch-specific install script for the stuff I feel like I need
+pacman -S git
+
+git clone https://github.com/syntactician/pkgbuild.git   ~/.pkgbuild
+git clone https://github.com/syntactician/dotfiles.git   ~/.dotfiles
+git clone https://github.com/syntactician/coursework.git ~/courses
+git clone https://github.com/syntactician/honors.git     ~/honors
+
+mkdir ~/.build
+git clone https://aur.archlinux.org/cower-git.git  ~/.build/cower-git  && cd ~/.build/cower-git  && makepkg -sri
+git clone https://aur.archlinux.org/pacaur-git.git ~/.build/pacaur-git && cd ~/.build/pacaur-git && makepkg -sri
+
+for pkgbuild in ~/.pkgbuild/*-git; do
+	mkdir ~/.build/$(basename $pkgbuild)
+	ln $pkgbuild ~/.build/$(basename $pkgbuild)/PKGBUILD
+done
+
+pacaur -S 
