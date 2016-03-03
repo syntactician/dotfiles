4ef98d1e5b6c14715e424b18c0f71f5482a21ac9
Edward Hernandez <ehernandez@email.wm.edu>
2016-03-03 10:28:47 -0500

    cleared out stuff i don't want

--- slock.config.def.h
+++
@@ -1,8 +0,0 @@
-static const char *colorname[NUMCOLS] = {
-	"#2b303b",     /* after initialization */
-	"#65737e",   /* during input */
-	"#bf616a",   /* wrong password */
-};
-
-/* treat a cleared input like a wrong password */
-static const int failonclear = 1;
