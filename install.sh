d3f4f17a2c7b1898fd498a9ef62b179b88996748
Edward Hernandez <ehernandez@email.wm.edu>
2016-04-01 21:17:56 -0400

    now using stow, etc.

--- install.sh
+++ install.sh
@@ -1,64 +1,65 @@
 #!/bin/sh
-# this is specific to arch linux and assummes it is in the same directory as
-# the configs i'm shipping
-mkdir ~/.config ~/.config/dunst ~/.config/mutt ~/.config/nvim
-ln dunstrc  ~/.config/dunst/dunstrc
-ln init.vim ~/.config/nvim/init.vim
-ln sxhkdrc  ~/.sxhkdrc
-ln xinitrc  ~/.xinitrc
-ln zshrc    ~/.zshrc
-cp muttrc   ~/.config/mutt/muttrc
+# depends on GNU Stow
 
-# let's get some fucking locales before we try to use anything
+[ -d ~/.config ] || mkdir ~/.config
+
+# let's make sure that UTF-8 is enabled
 sed -e '/en_US.UTF-8/s/#//' -i /etc/locale-gen
 locale-gen
 
-# an Arch-specific install script for the stuff I feel like I need
-pacman -S git
-
-git config --global user.email "edward.hernandez@gmx.com"
-git config --global user.name "Edward Hernandez"
-
-# git clone https://github.com/syntactician/dotfiles.git   ~/.dotfiles
-git clone https://github.com/syntactician/pkgbuild.git   ~/.pkgbuild
-
-git clone https://github.com/syntactician/coursework.git ~/courses
-git clone https://github.com/syntactician/honors.git     ~/honors
-
-# install pacaur to handle AUR installations
-mkdir ~/.build
-git clone https://aur.archlinux.org/cower-git.git  ~/.build/cower-git  && cd ~/.build/cower-git  && makepkg -sri
-git clone https://aur.archlinux.org/pacaur-git.git ~/.build/pacaur-git && cd ~/.build/pacaur-git && makepkg -sri
-
-# install all my PKGBUILDs programmatically
-for PKGBUILD in ~/.pkgbuild/*-git; do
-	mkdir ~/.build/$(basename $PKGBUILD)
-	ln $PKGBUILD ~/.build/$(basename $PKGBUILD)/PKGBUILD
-	(
-		cd ~/.build/$(basename $PKGBUILD)/
-		makepkg -sri
-	)
-done
-
-# enable testing repos
+# setup pacman for testing repos
 sed -e '/#[testing]/s/#//'                                                 \
 	-e '/[testing]/{n;s/.*/Include = /etc/pacman.d/mirrorlist/}'           \
 	-e '/#[community-testing/s/#//'                                        \
 	-e '/[community-testing/{n;s/.*/Include = /etc/pacman.d/mirrorlist/}'  \
 	-i /etc/pacman.conf
+pacman -Syu
+pacman -S stow
+
+# install pacaur to handle AUR installations
+[ -d ~/.build ] || mkdir ~/.build
+PKGS="cower-git pacaur-git"
+
+for PKG in $PKGS; do
+	git clone https://aur.archlinux.org/${PKG}.git ~/.build/$PKG
+	(
+		cd ~/.build/$PKG
+		makepkg -sri
+	)
+done
+
+# install my custom PKGBUILDs
+git clone https://github.com/syntactician/pkgbuild   ~/.build/.pkgbuild
+(
+	cd ~/.build/.pkgbuild
+	for PKG in */; do
+		stow $PKG
+		(
+			cd ../$PKG
+			makepkg -sri
+		)
+	done
+)
+
+# install my dotfiles
+for CONFIG in */; do
+	stow $CONFIG
+done
 
 pacaur -S \
-	dash libreoffice-fresh pandoc ruby texlive-most ttf-liberation \
+	dash libreoffice-fresh pandoc texlive-most ttf-liberation \
 	virtualbox-guest-utils xorg-server xorg-xinit \
 	dunst-git firefox-nightly hsetroot lemonbar-xft-git llpp wmutils-git \
 	neovim-git \
 
 # let's get a functional editor
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln ~/.dotfiles/init.vim ~/.config/nvim/init.vim
+nvim -c "call PlugFetch()" -c "PlugInstall"
 # i'll have to exit this manually. blah.
-nvim -c "PlugInstall"
 
-gem install gist
-gist --login
+# configure git
+git config --global user.email "edward.hernandez@gmx.com"
+git config --global user.name "Edward Hernandez"
+
+# get my work
+git clone https://github.com/syntactician/coursework.git ~/courses
+git clone https://github.com/syntactician/honors.git     ~/honors
